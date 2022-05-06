35ee56c899b38e6413ff7f9a04993b20647bcfe3
reirysson <reiryssonjp@gmail.com>
2022-05-06 17:13:32 -0300

    aula 06

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
 
 function App() {
 
   return (
     <div className="App">
+      <Frase />
       <SayMyName nome="Reirysson"/>
       <Pessoa 
         nome="Reirysson" 
