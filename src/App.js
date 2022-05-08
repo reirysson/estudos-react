1ea6c58c703cf15033e2c1636aa45583ac348245
reirysson <reiryssonjp@gmail.com>
2022-05-08 18:34:58 -0300

    aula 11

--- src/App.js
+++ src/App.js
@@ -3,15 +3,13 @@ import Evento from './components/Evento'
 import Form from './components/Form'
 
 function App() {
-
   return (
     <div className="App">
       <h1>Testando eventos</h1>
-      <Evento numero="1" />
-      <Evento numero="2" />
+      <Evento />
       <Form />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
